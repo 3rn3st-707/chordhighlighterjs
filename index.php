b57bfb92e2199c7fa9433f57ea56cc4979377530
ernest} <ernestoroberts7@yahoo.com>
2024-06-12 15:12:41 -0300

    commit mit

--- index.php
+++ index.php
@@ -1,23 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  
+<head> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Funciones Armonicas</title>
-
-    
 <?php
 header('Content-Type: text/html; charset=utf-8');
 ?>
-
 <link rel="stylesheet" type="text/css" href="styles/style.css">
-
 </head>
-
 <body>
-
-
 
 <div class="container">
 
