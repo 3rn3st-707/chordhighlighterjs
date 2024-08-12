4e60fe755071f21e6690bf410b1caa1e079f87c9
ernest} <ernestoroberts7@yahoo.com>
2024-08-11 22:31:48 -0300

    fn to .click() submit btn from any radio button

--- create.php
+++
@@ -1,9 +0,0 @@
-<?php
-  header('Content-Type: text/html; charset=utf-8');
-  include 'db.php';
-  $texto = $conn->real_escape_string($_POST["texto"]);
-  $sql = "insert into tabla1 (texto) values ('$texto')";
-  $conn->query($sql);
-  $conn->close();
-  header("location: tab.php");
-?>
